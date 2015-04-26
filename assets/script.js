f42b3b78eee55fbbf9dd84ccf1fe51ec825262dd
Julia Liang <julia.liang@berkeley.edu>
2015-04-26 10:08:44 -0700

    very buggy sad

---
+++ assets/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+	$('#content').waypoint(function(direction){
+		if (direction == "down") {
+			$('#nav').addClass('sticky');
+		} else {
+			$('#nav').removeClass('sticky');
+		}
+	});
+
+	$(document).ready(function(){
+	
+	$(".thumbnail").click(function() {
+		$("#modal-container").show();
+	});
+
+	$("#exit").click(function() {
+		$("#modal-container").hide();
+	});
+
+
+	$("#modal-overlay").click(function() {
+		$("#modal-container").hide();
+	});
+
+
+	// $(window).resize(function() {
+	// 	vertCenter();
+	// });
+
+	// function vertCenter() {
+	// 	var newHeight = $('#me').parent('#about-content-container').height();
+	// 	$('#photo').height(newHeight);
+	// 	$('#text').height(newHeight);
+	// }
+
+});
